805b326cb0999f20b6561efb63c0c2eb427791e8
Your Name <you@example.com>
2016-06-06 17:03:46 +1000

    dblab12

--- dbLab/my.sql
+++ dbLab/my.sql
@@ -28,3 +28,47 @@ SELECT emp.EmployeeID, emp.EmployeeName  FROM Employee emp INNER JOIN Employee b
 
 #7!!!
 SELECT wrk.EmployeeName, wrk.DepartmentID,  boss.EmployeeName  FROM Employee wrk INNER JOIN Employee boss  ON wrk.BossID = boss.EmployeeID  WHERE wrk.DepartmentID = boss.DepartmentID;
+
+#8!!!
+SELECT wrk.EmployeeName, wrk.EmployeeSalary, 
+boss.EmployeeName 
+FROM Employee wrk INNER JOIN Employee boss 
+ON wrk.BossID = boss.EmployeeID 
+WHERE wrk.DepartmentID = 11
+AND wrk.EmployeeSalary > 25000; 
+
+#9~~~
+SELECT EmployeeName, EmployeeSalary FROM Employee 
+WHERE EmployeeSalary > 
+(SELECT MAX(EmployeeSalary)  
+FROM Employee NATURAL JOIN Department 
+WHERE DepartmentName = "Marketing "); 
+
+#10!!!
+SELECT EmployeeName, EmployeeSalary FROM Employee 
+WHERE EmployeeID IN 
+(SELECT BossID FROM Employee 
+GROUP BY BossID  
+HAVING COUNT(*) > 2); 
+
+#11!!!
+SELECT SupplierID, SupplierName FROM Supplier 
+WHERE SupplierID NOT IN 
+(SELECT SupplierID FROM Delivery  
+NATURAL JOIN Item  
+WHERE ItemName = 'Compass'); 
+
+#12!!!
+SELECT Supplier.SupplierID, SupplierName FROM Delivery 
+NATURAL JOIN Supplier NATURAL JOIN Item 
+WHERE ItemType = 'N' 
+GROUP BY Supplier.SupplierID, SupplierName 
+HAVING COUNT(DISTINCT Delivery.ItemID) > 1; 
+ 
+SELECT Supplier.SupplierID, SupplierName, count(Distinct Delivery.ItemID) as NumItems  
+FROM Delivery NATURAL JOIN Supplier NATURAL JOIN Item 
+WHERE ItemType = 'N' 
+GROUP BY Supplier.SupplierID, SupplierName 
+HAVING NumItems > 1; 
+
+#13
